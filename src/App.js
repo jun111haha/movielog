8ea07115f6b8c6a9819ebf0a4f8fc8017f944eb0
jun111haha <jun111haha@gmail.com>
2022-01-15 18:35:12 +0900

    v 0.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./scss/base.scss";
+import Banner from "./component/Banner.js";
+import Nav from "./component/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Nav/>
+      <Banner/>
+
+
     </div>
+
   );
 }
 
