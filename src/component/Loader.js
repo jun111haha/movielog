375fa873a36bd46d763638f74d5bdc4f4b38b9f3
kj <jun111haha@gmail.com>
2022-07-07 00:27:50 +0900

    Loader 생성 / 무한스크롤 page 설정

---
+++ src/component/Loader.js
@@ -0,0 +1,22 @@
+import ReactLoading from "react-loading";
+import styled from "styled-components";
+import React, { memo } from "react";
+
+const LoaderWrap = styled.div`
+    width : 100%;
+    height : 80%
+    display : flex;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+`;
+
+const Loader = () => {
+  return (
+    <LoaderWrap>
+      <ReactLoading type="spin" color="#A593E0"></ReactLoading>
+    </LoaderWrap>
+  );
+};
+
+export default memo(Loader);
--- src/views/TV/TvContainer.js
+++ src/views/TV/TvContainer.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { tvApi } from "../../Api";
 import TvPresenter from "./TvPresenter";
+import { useInView } from "react-intersection-observer";
 
 const TvContainer = (props) => {
   const {
@@ -10,32 +11,26 @@ const TvContainer = (props) => {
   const [topRated, setTopRated] = useState([]);
   const [popular, setPopular] = useState([]);
   const [airingToday, setAiringToday] = useState([]);
-  const [netflixOriginals, setNetflixOriginals] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   async function fetchData() {
     try {
       const {
         data: { results: topRatedRequest },
-      } = await tvApi.topRated();
+      } = await tvApi.topRated(page);
 
       const {
         data: { results: topPopularRequest },
-      } = await tvApi.popular();
-
-      // const {
-      //   data: { results: netflixOriginals },
-      // } = await tvApi.netflixOriginals();
+      } = await tvApi.popular(page);
 
       const {
         data: { results: airingToday },
-      } = await tvApi.airingToday();
+      } = await tvApi.airingToday(page);
 
       setTopRated(topRatedRequest);
       setPopular(topPopularRequest);
       setAiringToday(airingToday);
-      // setNetflixOriginals(netflixOriginals);
-
       setLoading(false);
     } catch (error) {
       console.log(error);
